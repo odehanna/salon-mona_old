7758b900e173cef1e5713f61e2604d5bac579ad8
odehanna <odeh.anna@gmail.com>
2014-02-28 19:38:49 -0500

    aa

---
+++ _site/form.php
@@ -0,0 +1,24 @@
+<?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $text = $_POST['text'];
+    $from = 'From: Salon-Mona'; 
+    $to = 'odeh.anna@gmail.com'; 
+    $subject = 'Hello';
+
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+?>
+
+<?php
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) { 
+        echo '<p>Your message has been sent!</p>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    }
+    else {
+        echo '<p>You need to fill in all required fields!!</p>';
+    }
+?>
+}
+?>
