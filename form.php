739850f40aafa99d8770a897d66ea00dae8b21ed
odehanna <odeh.anna@gmail.com>
2014-03-10 11:34:32 -0400

    a

--- form.php
+++ form.php
@@ -1,13 +1,17 @@
 <?php
+
+error_reporting(-1);
+ini_set('display_errors', 'on');
+
     $name = $_POST['name'];
     $email = $_POST['email'];
     $text = $_POST['text'];
-    $from = 'From: Salon-Mona'; 
+    $from = "From: $name <$email>"; 
     $to = 'odeh.anna@gmail.com'; 
     $subject = 'Hello';
 
-    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
-
+    $body = "From: $name\n E-Mail: $email\n Message:\n $text";
+/*
 if ($_POST['submit']) {
            if (mail ($to, $subject, $body, $from)) {
             header('Location: /thank-you/');
@@ -15,48 +19,34 @@ if ($_POST['submit']) {
            } else {
            echo '<p>Ah! Try again, please?</p>';
            }
-       }
+       }*/
 
 $errors = '';
-$myemail = 'odeh.anna@gmail.com';//<-----Put Your email address here.
+
 if(empty($_POST['name'])  || 
    empty($_POST['email']) || 
-   empty($_POST['message']))
+   empty($_POST['text']))
 {
     $errors .= "\n Error: all fields are required";
 }
 
 if (!preg_match(
 "/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/i", 
-$email_address))
+$email))
 {
     $errors .= "\n Error: Invalid email address";
 }
 
 if( empty($errors))
- 
 {
- 
-$to = $myemail;
- 
-$email_subject = "Contact form submission: $name";
- 
-$email_body = "You have received a new message. ".
- 
-" Here are the details:\n Name: $name \n ".
- 
-"Email: $email_address\n Message \n $message";
- 
-$headers = "From: $myemail\n";
- 
-$headers .= "Reply-To: $email_address";
- 
-mail($to,$email_subject,$email_body,$headers);
+
+mail($to,$subject,$body,$from);
  
 //redirect to the 'thank you' page
+header('Location: http://salon-mona.com/thank-you/');
+exit;
  
-header('Location: contact-form-thank-you.html');
- 
+} else {
+  echo "Not valid: $errors";
 }
- ?>
 
